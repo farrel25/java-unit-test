bd9006680904559799f8c57af40649678e7201a4
Farrel Putra <frl25ap@gmail.com>
2023-04-01 14:16:19 +0700

    Add method that run before and after all and each unit test is run

--- src/test/java/farrel/com/unittest/CalculatorTest.java
+++ src/test/java/farrel/com/unittest/CalculatorTest.java
@@ -1,10 +1,8 @@
 package farrel.com.unittest;
 
 import farrel.com.unittest.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayName("Test For Calculator Class")
@@ -12,6 +10,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After each");
+    }
 
     @Test
     //@DisplayName("Test Success Scenario of method add(Integer, Integer)")
