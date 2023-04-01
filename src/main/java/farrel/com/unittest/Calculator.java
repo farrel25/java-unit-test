6072fca1fcb37c75e235d0358b618442fe792604
Farrel Putra <frl25ap@gmail.com>
2023-04-01 12:50:57 +0700

    Add implementation of Assertions

--- src/main/java/farrel/com/unittest/Calculator.java
+++ src/main/java/farrel/com/unittest/Calculator.java
@@ -1,8 +1,17 @@
 package farrel.com.unittest;
 
+import java.io.IOException;
+
 public class Calculator {
 
     public Integer add(Integer firstNumber, Integer secondNumber) {
         return firstNumber + secondNumber;
+    }
+
+    public Integer divide(Integer number, Integer divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Can not divide by zero");
+        }
+        return number/divisor;
     }
 }
--- src/test/java/farrel/com/unittest/CalculatorTest.java
+++ src/test/java/farrel/com/unittest/CalculatorTest.java
@@ -1,6 +1,7 @@
 package farrel.com.unittest;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -9,5 +10,25 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess() {
         Integer result = calculator.add(10, 20);
+
+        // if we don't use assertions, we can user use if else, but it's not recommended
+//        if (result != 30) {
+//            throw new RuntimeException("Test gagal");
+//        }
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testDivideSuccess() {
+        Integer result = calculator.divide(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDivideFailed() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.divide(10, 0)
+        );
     }
 }
