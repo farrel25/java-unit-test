248c24c1648ee2e555393b811c610487630dd2f0
Farrel Putra <frl25ap@gmail.com>
2023-04-02 11:59:52 +0700

    Change test lifecycle

--- src/test/java/farrel/com/unittest/OrderedTest.java
+++ src/test/java/farrel/com/unittest/OrderedTest.java
@@ -1,31 +1,44 @@
 package farrel.com.unittest;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 //@TestMethodOrder(value = MethodOrderer.MethodName.class)
 //@TestMethodOrder(value = MethodOrderer.Random.class)
 //@TestMethodOrder(value = MethodOrderer.DisplayName.class)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 public class OrderedTest {
+
+    private int counter = 0;
+
+    @BeforeAll
+    void beforeAll() {
+
+    }
+
+    @AfterAll
+    void afterAll() {
+
+    }
 
     @Test
     @Order(value = 1)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(value = 3)
     void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(value = 2)
     void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
 }
