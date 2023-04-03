51da2796c4f6e355534b03edbb6e5fdfb64e72b1
Farrel Putra <frl25ap@gmail.com>
2023-04-03 07:31:14 +0700

    Add implementation of unit test loops

--- src/test/java/farrel/com/unittest/RandomCalculatorTest.java
+++ src/test/java/farrel/com/unittest/RandomCalculatorTest.java
@@ -1,8 +1,7 @@
 package farrel.com.unittest;
 
 import farrel.com.unittest.resolver.RandomParameterResolver;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 
@@ -18,6 +17,26 @@ public class RandomCalculatorTest extends AbstractCalculatorTest {
 
     @Test
     void testRandom(Random random) {
+        int a = random.nextInt();
+        int b = random.nextInt();
+
+        Integer result = calculator.add(a, b);
+        int expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName(value = "Test random calculator")
+    @RepeatedTest(
+            value = 10,
+            //name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+            name = "{displayName}"
+    )
+    void testRandomRepeat(TestInfo testInfo, Random random, RepetitionInfo repetitionInfo) {
+        System.out.println(
+                testInfo.getDisplayName() + " ke " + repetitionInfo.getCurrentRepetition() + " dari " + repetitionInfo.getTotalRepetitions()
+        );
+
         int a = random.nextInt();
         int b = random.nextInt();
 
